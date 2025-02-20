30d560692fe432bea0198151e68345a9a983572d
luuhuy2402 <xeoxeo1895@gmail.com>
2025-02-20 08:53:19 +0700

    Code App bar

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+
+function Profiles() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <Box>
+            <Tooltip title="Account settings">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ padding: 0 }}
+                    aria-controls={open ? "basic-menu-profiles" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                >
+                    <Avatar
+                        sx={{ width: 30, height: 30 }}
+                        alt="Avatar"
+                        src="https://hoanghamobile.com/tin-tuc/wp-content/webp-express/webp-images/uploads/2024/04/hinh-anh-de-thuong-2.jpg.webp"
+                    />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                id="basic-menu-profiles"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    "aria-labelledby": "basic-button-profiles",
+                }}
+            >
+                <MenuItem>
+                    <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> Profile
+                </MenuItem>
+                <MenuItem>
+                    <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                    <ListItemIcon>
+                        <PersonAdd fontSize="small" />
+                    </ListItemIcon>
+                    Add another account
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </Box>
+    );
+}
+
+export default Profiles;
--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,23 @@
+import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
 const theme = extendTheme({
     custom: {
-        appBarHeight: "48px",
-        boardBarHeight: "58px",
+        appBarHeight: "58px",
+        boardBarHeight: "60px",
     },
     colorSchemes: {
         light: {
             palette: {
-                primary: {
-                    main: "#66cfbb",
-                },
+                primary: teal,
+                secondary: deepOrange,
             },
             // spacing: (factor) => `${0.25 * factor}rem`, //cuastom spacing
         },
         dark: {
             palette: {
-                primary: {
-                    main: "#fff",
-                },
+                primary: cyan,
+                secondary: orange,
             },
             // spacing: (factor) => `${0.25 * factor}rem`,
         },
