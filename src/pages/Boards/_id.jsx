8799ba0527b49df0e677e6a6fdeffa0b65c5ac0d
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-20 10:13:39 +0700

    Axios: GáCall API

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,16 +1,26 @@
 // Board details
 import { Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import { fetchBoardDetailsAPI } from "~/apis";
 import { mockData } from "~/apis/mock-data";
 import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "~/pages/Boards/BoardBar/BoardBar";
 import BoardContent from "~/pages/Boards/BoardContent/BoardContent";
 
 function Board() {
+    const [board, setBoard] = useState(null);
+    useEffect(() => {
+        const boardId = "67db791c371aabf3a50e3a5a";
+        //Call API
+        fetchBoardDetailsAPI(boardId).then((board) => {
+            setBoard(board);
+        });
+    }, []);
     return (
         <Container disableGutters maxWidth="false" sx={{ height: "100vh" }}>
             <AppBar />
-            <BoardBar board={mockData?.board} />
-            <BoardContent board={mockData?.board} />
+            <BoardBar board={board} />
+            <BoardContent board={board} />
         </Container>
     );
 }
